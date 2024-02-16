3b3fead9112e15b8c2d19b39c7c67196113fac17
Prathamesh <prathameshchilame@gmail.com>
2024-02-16 19:35:20 +0530

    recaptcha

--- script.js
+++ script.js
@@ -12,11 +12,13 @@ function beforesubmit(event){
         event.preventDefault();
     }
 }
+
 function timestamp(){
     var response = document.getElementById("g-recaptcha-response"); 
-    if (response == null || response.value.trim() == "") 
-        {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
-        elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+    if (response == null || response.value.trim() == "") {
+        var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+        elems["ts"] = JSON.stringify(new Date().getTime());
+        document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
     } 
 } 
 setInterval(timestamp, 500); 
